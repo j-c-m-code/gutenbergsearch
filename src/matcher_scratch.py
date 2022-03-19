87f1bd5ed43ff97b01069cdead64bc6cc15b3cff
Jamie Martin <james.campbell.martin@gmail.com>
2022-03-19 12:42:52 -0400

    blah

--- src/matcher_scratch.py
+++ src/matcher_scratch.py
@@ -7,9 +7,26 @@ from spacy.matcher import Matcher
 import terms as tm
 
 TEST_TEXT = """
-His eyes roamed her face. This house is bigger than it looks, for it slides for two storeys down the hill behind, and the wooden door, which is always locked, really leads into the attic. The knowing person prefers to follow the precipitous mule-track round the turn of the mud wall till he can take the edifice in the rear. Then--being now on a level with the cellars--he lifts up his head and shouts. If his voice sounds like something light--a letter, for example, or some vegetables, or a bunch of flowers--a basket is let out of the first-floor windows by a string, into which he puts his burdens and departs. But if he sounds like something heavy, such as a log of wood, or a piece of meat, or a visitor, he is interrogated, and then bidden or forbidden to ascend. The ground floor and the upper floor of that battered house are alike deserted, and the inmates keep the central portion, just as in a dying body all life retires to the heart. There is a door at the top of the first flight of stairs, and if the visitor is admitted he will find a welcome which is not necessarily cold. He touched her face softly, then her eyes. There are several rooms, some dark and mostly stuffy--a reception-room adorned with horsehair chairs, wool-work stools, and a stove that is never lit--German bad taste without German domesticity broods over that room; also a living-room, which insensibly glides into a bedroom when the refining influence of hospitality is absent, and real bedrooms; and last, but not least, the loggia, where you can live day and night if you feel inclined, drinking vermouth and smoking cigarettes, with leagues of olive-trees and vineyards and blue-green hills to watch you.
+His eyes roamed her face and her eyes. I touch your ear.
 
 """
+
+
+def on_match(matcher, doc, id, matches):
+    print(matches)
+    print(sent)
+    to_label = yes_or_no()
+    print(to_label)
+
+
+def yes_or_no():
+    while "the answer is invalid":
+        reply = str(input("Label as touch? (y/n) > ")).lower().strip()
+        if reply[:1] == "y":
+            return True
+        if reply[:1] == "n":
+            return False
+
 
 nlp = spacy.load("en_core_web_lg")
 
@@ -36,12 +53,11 @@ verb_pattern = [
     {"LEMMA": {"IN": tm.bodypart_list}, "DEP": "dobj"},
 ]
 
-matcher.add("VERB_PATTERN", [verb_pattern])
-
-matcher.add("BODY_PATTERN", [body_pattern])
+matcher.add("PATTERNS", [verb_pattern, body_pattern], on_match=on_match)
 
 sentences = list(doc.sents)
 
 for sent in sentences:
-    matches = matcher(sent)
-    breakpoint()
+    # Spacy matcher works on a Doc or a Span (calling with a Span here)
+    matches = matcher(sent, as_spans=True)
+#    breakpoint()
