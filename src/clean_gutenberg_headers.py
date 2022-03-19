5eab1df5850cc798df9f0971a56879c7fd5fad66
Jamie Martin <james.campbell.martin@gmail.com>
2022-03-19 16:25:15 -0400

    Simplified; I don't need the legalese part

--- src/clean_gutenberg_headers.py
+++ src/clean_gutenberg_headers.py
@@ -3,8 +3,6 @@ Removes lines that are part of the Project Gutenberg header or footer.
 """
 from gutenberg_strings import TEXT_START_MARKERS
 from gutenberg_strings import TEXT_END_MARKERS
-from gutenberg_strings import LEGALESE_START_MARKERS
-from gutenberg_strings import LEGALESE_END_MARKERS
 
 
 def strip_headers(text):
@@ -20,43 +18,12 @@ def strip_headers(text):
     lines = text.splitlines(True)  # True option means keep line endings
 
     out = []
-    i = 0
-    footer_found = False
-    ignore_section = False
 
     for line in lines:
-        reset = False
-
-        if i <= 600:
-            # Check if the header ends here
-            if any(line.startswith(token) for token in TEXT_START_MARKERS):
-                reset = True
-
-            # If it's the end of the header, delete the output produced so far.
-            # May be done several times, if multiple lines occur indicating the
-            # end of the header
-            if reset:
-                out = []
-                continue
-
-        if i >= 100:
-            # Check if the footer begins here
-            if any(line.startswith(token) for token in TEXT_END_MARKERS):
-                footer_found = True
-
-            # If it's the beginning of the footer, stop output
-            if footer_found:
-                break
-
-        if any(line.startswith(token) for token in LEGALESE_START_MARKERS):
-            ignore_section = True
-            continue
-        elif any(line.startswith(token) for token in LEGALESE_END_MARKERS):
-            ignore_section = False
-            continue
-
-        if not ignore_section:
+        if any(line.startswith(token) for token in TEXT_END_MARKERS):
+            break
+        elif any(line.startswith(token) for token in TEXT_START_MARKERS):
+            out = []
+        else:
             out.append(line)
-            i += 1
-
     return "".join(out)
