94a157906d45a5eb71985765281fbf32d7e7d251
Jamie Martin <james.campbell.martin@gmail.com>
2022-03-20 15:45:04 -0400

    Linter suggestions

--- src/use_matcher.py
+++ src/use_matcher.py
@@ -48,11 +48,15 @@ filelist = glob.glob("*")
 
 
 def process_match(sentence):
+    """
+    Is this a real match?
+    If yes, write it to the txt file
+    """
     print(sentence)
     to_label = yes_or_no()
     if to_label:
         with open(
-            rf"C:\Users\james\testing use_matcher output.txt",
+            rf"C:\Users\james\{short_name} use_matcher output.txt",
             "w",
             encoding="utf-8",
         ) as writer:
@@ -73,31 +77,9 @@ def yes_or_no():
             return True
         if reply[:1] == "n":
             return False
-    with open(
-        rf"C:\Users\james\{short_name} use_matcher output.txt", "w", encoding="utf-8"
-    ) as writer:
-        writer.write("Match number " + str(COUNTER) + "\n")
-        writer.write("from " + str(Path(filename)) + "\n")
-        # sent is a Spacy span object.
-        # ask for its text attribute to get the text
-        writer.write(sent.text)
-        writer.write("\n\n")
 
 
 for filename in filelist:
-
     doc = Doc(nlp.vocab).from_disk(filename)
-
     sentences = list(doc.sents)
-
-    COUNTER = 0
-
     short_name = Path(filename).stem
-
-    for sent in sentences:
-        # Spacy matcher works on a Doc or a Span (calling with a Span here)
-        matches = matcher(sent)
-        if len(matches) > 0:
-            # sent.text sends only the text of the sentence,
-            # not the Spacy object
-            process_match(sent.text)
