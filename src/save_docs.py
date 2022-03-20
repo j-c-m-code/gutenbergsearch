eb6914e33a9ff07b588562215a1fd03e8e85a43a
Jamie Martin <james.campbell.martin@gmail.com>
2022-03-20 17:36:04 -0400

    fixes

--- src/save_docs.py
+++ src/save_docs.py
@@ -1,29 +1,35 @@
 """
-Processes .txt file to Spacy doc then saves the doc
+Processes a folder of .txt files to Spacy docs then saves the docs
 """
 # first import standard modules
+import glob
+import os
 from pathlib import Path
 
 # then import third-party modules
 import spacy
 
 # finally import my own code (PEP-8 convention)
-from askfile import whichfile
+from askdir import whichdir
 
 nlp = spacy.load("en_core_web_lg")
 
-filename = whichfile()
-short_name = Path(filename).stem
+directoryname = whichdir()
+os.chdir(directoryname)
+filelist = glob.glob("*")
 
-with open(filename, "r", encoding="utf-8") as f:
-    novel = f.read()
+for filename in filelist:
+    with open(filename, "r", encoding="utf-8") as f:
+        novel = f.read()
 
-# the novel is too long for the default, so increase allocated memory
-nlp.max_length = len(novel) + 100
+    # the novel is too long for the default, so increase allocated memory
+    nlp.max_length = len(novel) + 100
 
-# Process a text
-doc = nlp(novel)
+    # Process a text
+    doc = nlp(novel)
 
-# r for raw string--no escape characters
-# f for format string--allow me to pass in variable
-doc.to_disk(rf"C:\Users\james\{short_name}")
+    short_name = Path(filename).stem
+
+    # r for raw string--no escape characters
+    # f for format string--allow me to pass in variable
+    doc.to_disk(rf"C:\Users\james\{short_name}")
