d9a150b72a84ab0e6c2af86465211f3e0cad0cc4
Jamie Martin <james.campbell.martin@gmail.com>
2022-03-29 13:46:57 -0400

    blah

--- src/scratch.py
+++ src/scratch.py
@@ -3,6 +3,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as pyplot
 
-# Simple tests
-print("Hello!")
-17 + 27
+import askfile as af
+
+# <codecell>
+file_to_read = af.whichfile()
+
+# <codecell>
+data_frame = pd.read_csv(file_to_read, nrows=35, encoding="UTF-8", usecols=["text"])
+texts = [_ for _ in data_frame["text"]]
