f7cab35fc992a850a6a4b68f4eeef47a34e35ce4
Jamie Martin <james.campbell.martin@gmail.com>
2022-03-29 14:47:30 -0400

    added cells

--- src/scratch.py
+++ src/scratch.py
@@ -6,8 +6,18 @@ import matplotlib.pyplot as pyplot
 import askfile as af
 
 # <codecell>
+import spacy
+from spacy import displacy
+
+nlp = spacy.load("en_core_web_lg")
+
+# <codecell>
 file_to_read = af.whichfile()
 
 # <codecell>
 data_frame = pd.read_csv(file_to_read, nrows=35, encoding="UTF-8", usecols=["text"])
 texts = [_ for _ in data_frame["text"]]
+
+doc = nlp("What is a cat?")
+displacy.render(doc)
+spacy.explain("AUX")
