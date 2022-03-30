bd0c650fe4ee56b93438892e06be672e385d2ae6
Jamie Martin <james.campbell.martin@gmail.com>
2022-03-30 14:11:52 -0400

    trial version working

--- src/scratch.py
+++ src/scratch.py
@@ -1,23 +1,30 @@
 # <codecell>
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as pyplot
+import numpy
+import pandas
+import matplotlib.pyplot
 
-import askfile as af
-
-# <codecell>
 import spacy
-from spacy import displacy
 
+import askfile
+import use_matcher
+
+# <codecell>
 nlp = spacy.load("en_core_web_lg")
+matcher = spacy.matcher.Matcher(nlp.vocab)
 
 # <codecell>
-file_to_read = af.whichfile()
+file_to_read = askfile.whichfile()
 
 # <codecell>
-data_frame = pd.read_csv(file_to_read, nrows=35, encoding="UTF-8", usecols=["text"])
+data_frame = pandas.read_csv(file_to_read, nrows=35, encoding="UTF-8", usecols=["text"])
 texts = [_ for _ in data_frame["text"]]
 
-doc = nlp("What is a cat?")
-displacy.render(doc)
-spacy.explain("AUX")
+# <codecell>
+matcher.add("PATTERNS", [use_matcher.subject_pattern, use_matcher.object_pattern])  # type: ignore
+for text in texts:
+    doc = nlp(text)
+    matches = matcher(doc)
+    if len(matches) > 0:
+        print("TRUE " + doc.text + "\n")
+    else:
+        print("FALSE " + doc.text + "\n")
