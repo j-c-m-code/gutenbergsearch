b5801fc4e6701e4d587afeebf8016d54896f56cd
Jamie Martin <james.campbell.martin@gmail.com>
2022-04-02 08:44:59 -0400

    format changes

--- src/scratch.py
+++ src/scratch.py
@@ -1,11 +1,11 @@
 # <codecell>
 import csv
-import numpy
+import os
 import pandas
-import matplotlib.pyplot
 
 import spacy
 
+import askdir
 import askfile
 import use_matcher
 
@@ -28,6 +28,10 @@ matcher.add(
     "PATTERNS", [use_matcher.subject_pattern, use_matcher.object_pattern]
 )  # type: ignore
 
+output_directory = askdir.whichdir()
+
+os.chdir(output_directory)
+
 with open("test_output.csv", "a") as file:
     writer = csv.writer(file)
     writer.writerow(["text", "label", "result"])
@@ -35,6 +39,6 @@ with open("test_output.csv", "a") as file:
         doc = nlp(text)
         matches = matcher(doc)
         if len(matches) > 0:
-            writer.writerow([text, labels[count], "TRUE"])
+            writer.writerow([text, labels[count], "Positive"])
         else:
-            writer.writerow([text, labels[count], "FALSE"])
+            writer.writerow([text, labels[count], "Negative"])
