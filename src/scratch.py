3c7ca4bc35cdc98b3347662fd1b3f5b105e37b2f
Jamie Martin <james.campbell.martin@gmail.com>
2022-04-02 10:54:01 -0400

    changes

--- src/scratch.py
+++ src/scratch.py
@@ -37,14 +37,32 @@ os.chdir(output_directory)
 with open("test_output.csv", "a", newline="", encoding="UTF-8") as file:
     writer = csv.writer(file)
     writer.writerow(["text", "self_touch_actual", "self_touch_predicted"])
-    for count, text in enumerate(texts):
-        doc = nlp(text)
+    for count, sentence in enumerate(sentences):
+        doc = nlp(sentence)
         matches = matcher(doc)
         if len(matches) > 0:
-            writer.writerow([text, self_touch_actual[count], "yes"])
+            writer.writerow([sentence, self_touch_actuals[count], "yes"])
         else:
-            writer.writerow([text, self_touch_actual[count], "no"])
+            writer.writerow([sentence, self_touch_actuals[count], "no"])
 
 # <codecell>
 output_file = askfile.whichfile()
 output_frame = pandas.read_csv(output_file, encoding="UTF-8")
+# mapping "yes" to 1 and "no" to 0 for display
+# output_frame["self_touch_actual"] = output_frame["self_touch_actual"].map(
+#     {"yes": 1, "no": 0}
+# )
+# output_frame["self_touch_predicted"] = output_frame["self_touch_predicted"].map(
+#     {"yes": 1, "no": 0}
+# )
+confusion_matrix = pandas.crosstab(
+    output_frame["self_touch_actual"],
+    output_frame["self_touch_predicted"],
+    rownames=["Actual"],
+    colnames=["Predicted"],
+)
+
+# group_names = ["True Neg", "False Pos", "False Neg", "True Pos"]
+
+seaborn.heatmap(confusion_matrix, annot=True, cmap="Blues")
+matplotlib.pyplot.show()
