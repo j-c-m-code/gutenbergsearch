95779c2a4e3c87135436cb0c366a9eaa1825a69d
Jamie Martin <james.campbell.martin@gmail.com>
2022-04-09 07:29:21 -0400

    added error

---
+++ src/unicode_error.py
@@ -0,0 +1,17 @@
+# <codecell>
+import csv
+import unicodedata
+
+test_char = "\u00e9"  # LATIN SMALL LETTER E WITH ACUTE
+
+print(test_char)
+
+with open("test.csv", "w", newline="", encoding="UTF-8") as file:
+    writer = csv.writer(file)
+    writer.writerow(test_char)
+
+# when I open test.csv in Atom, I see the correct character
+# but when I open test.csv in Excel on Windows 10, I see these two characters:
+
+unicodedata.name("Ã")  # LATIN CAPITAL LETTER A WITH TILDE
+unicodedata.name("©")  # COYRIGHT SIGN
