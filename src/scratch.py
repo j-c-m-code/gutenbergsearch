5643f2c2b22832690357c17d3f68276ba98f0949
Jamie Martin <james.campbell.martin@gmail.com>
2022-04-09 07:31:47 -0400

    test

--- src/scratch.py
+++ src/scratch.py
@@ -69,21 +69,3 @@ confusion_matrix = pandas.crosstab(
 )
 
 seaborn.heatmap(confusion_matrix, annot=True, cmap="Blues")
-
-# <codecell>
-import csv
-import unicodedata
-
-test_char = "\u00e9"  # LATIN SMALL LETTER E WITH ACUTE
-
-print(test_char)
-
-with open("test.csv", "w", newline="", encoding="UTF-8") as file:
-    writer = csv.writer(file)
-    writer.writerow(test_char)
-
-# when I open test.csv in Atom, I see the correct character
-# but when I open test.csv in Excel on Windows 10, I see these two characters:
-
-unicodedata.name("Ã")  # LATIN CAPITAL LETTER A WITH TILDE
-unicodedata.name("©")  # COYRIGHT SIGN
