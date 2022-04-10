f7719e353def2f6f1454e6282d39b3b9cb189be1
Jamie Martin <james.campbell.martin@gmail.com>
2022-04-10 18:59:49 -0400

    added comment

--- src/display_touch_stats.py
+++ src/display_touch_stats.py
@@ -10,8 +10,12 @@ input_frame = pandas.read_csv(input_file, encoding="UTF-8")
 
 print(input_frame)
 
-input_frame["self_touch"] = input_frame["true_positives"] / (
-    input_frame["false_positives"] + input_frame["all_negatives"]
+# multiplying numerator by 1000 for easier reading
+
+input_frame["self_touch"] = (
+    1000
+    * input_frame["true_positives"]
+    / (input_frame["false_positives"] + input_frame["all_negatives"])
 )
 
 input_frame.sort_values(by="self_touch", ascending=False)
