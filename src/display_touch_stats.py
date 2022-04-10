7957d78f7b4898fa2ae0069a379a05f348fa98ca
Jamie Martin <james.campbell.martin@gmail.com>
2022-04-10 18:59:01 -0400

    working

--- src/display_touch_stats.py
+++ src/display_touch_stats.py
@@ -1,9 +1,5 @@
-import csv
-import os
 import pandas
 
-import seaborn
-import spacy
 
 import askfile
 
@@ -11,3 +7,11 @@ print("Which CSV file for input?")
 input_file = askfile.whichfile()
 
 input_frame = pandas.read_csv(input_file, encoding="UTF-8")
+
+print(input_frame)
+
+input_frame["self_touch"] = input_frame["true_positives"] / (
+    input_frame["false_positives"] + input_frame["all_negatives"]
+)
+
+input_frame.sort_values(by="self_touch", ascending=False)
