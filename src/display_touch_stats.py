9408c327e851586d3dd3cc5bb150c83381fc2248
Jamie Martin <james.campbell.martin@gmail.com>
2022-04-10 20:14:15 -0400

    Changed denominator to word count instead of sentence count

--- src/display_touch_stats.py
+++ src/display_touch_stats.py
@@ -10,11 +10,11 @@ input_file = askfile.whichfile()
 
 input_frame = pandas.read_csv(input_file, encoding="UTF-8")
 
-# multiplying numerator by 1000 for easier reading
+print(input_frame)
+
+# multiplying numerator by 10000 for easier reading
 input_frame["self_touch"] = (
-    1000
-    * input_frame["true_positives"]
-    / (input_frame["false_positives"] + input_frame["all_negatives"])
+    10000 * input_frame["true_positives"] / (input_frame["word_count"])
 )
 
 input_frame.sort_values(by="self_touch", ascending=False)
