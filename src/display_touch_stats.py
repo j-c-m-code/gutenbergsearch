a77d4c65c95c3bfb951aa4ba4fd5a2080821036d
Jamie Martin <james.campbell.martin@gmail.com>
2022-04-11 15:52:47 -0400

    create results csv file

--- src/display_touch_stats.py
+++ src/display_touch_stats.py
@@ -10,11 +10,11 @@ input_file = askfile.whichfile()
 
 input_frame = pandas.read_csv(input_file, encoding="UTF-8")
 
-print(input_frame)
-
 # multiplying numerator by 10000 for easier reading
 input_frame["self_touch"] = (
     10000 * input_frame["true_positives"] / (input_frame["word_count"])
 )
 
 input_frame.sort_values(by="self_touch", ascending=False)
+
+input_frame.to_csv("out.csv", encoding="UTF-8")
